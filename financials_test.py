822c0792fafee875254f50e25ba18a11359280f9
Peter Matthews <pjdmathews7@gmail.com>
2024-05-15 16:35:47 -0400

    Income Statement

---
+++ financials_test.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+
+ticker = yf.Ticker("GE")
+
+income_df = ticker.income_stmt
+balance_df = ticker.balance_sheet
+cash_df = ticker.cashflow
+
+print("YFinance Income Statement\n")
+
+print(income_df)
+
+
+
