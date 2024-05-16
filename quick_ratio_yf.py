d7ab0273b6a120f2d5e6d2a8eabeaadeb616d79d
Peter <pjdmatthews7@gmail.com>
2024-05-16 09:26:46 -0400

    Quick Ratio

---
+++ quick_ratio_yf.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+import sys
+
+def get_quick(ticker_in):
+    ticker = yf.Ticker(ticker_in)
+    balance_df = ticker.balance_sheet
+    inventory = balance_df.loc['Inventory', balance_df.columns[0].strftime('%Y-%m-%d')]
+    current_assets = balance_df.loc['Current Assets', balance_df.columns[0].strftime('%Y-%m-%d')]
+    current_liabilities = balance_df.loc['Current Liabilities', balance_df.columns[0].strftime('%Y-%m-%d')]
+    quick_ratio = (current_assets - inventory)/current_liabilities
+    return float("{:.2f}".format(quick_ratio))
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python quick_ratio_yf.py TICKER")
+        sys.exit(1)
+
+    ticker = sys.argv[1]
+    current = get_quick(ticker)
+    print("Quick: ", current)
