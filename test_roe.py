580f7572156bc615d7fdeaa08c5bf78a3e29ffb1
Peter Matthews <pjdmatthews7@gmail.com>
2024-05-17 14:29:29 -0400

    Testing ROE

---
+++ test_roe.py
@@ -0,0 +1,13 @@
+import yfinance as yf
+import sys
+ticker = yf.Ticker("GE")
+balance_df = ticker.balance_sheet
+income_df = ticker.income_stmt
+    
+net_income = income_df.loc['Net Income', income_df.columns[0].strftime('%Y-%m-%d')]
+current_assets = balance_df.loc['Current Assets', balance_df.columns[0].strftime('%Y-%m-%d')]
+current_liabilities = balance_df.loc['Current Liabilities', balance_df.columns[0].strftime('%Y-%m-%d')]
+shareholder_equity = current_assets - current_liabilities
+
+print(net_income)
+print(shareholder_equity)
