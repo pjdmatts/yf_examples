b4779ffd453d9e89c86c0869fbdd2e9fabe7b659
Peter Matthews <pjdmatthews7@gmail.com>
2024-05-17 14:07:42 -0400

    Net Profit

---
+++ net_profit_yf.py
@@ -0,0 +1,20 @@
+import yfinance as yf
+import sys
+
+# Function calculates Net Profit Margin
+def get_profit_margin(ticker_in):
+    ticker = yf.Ticker(ticker_in)
+    income_df = ticker.income_stmt
+    net_profit = income_df.loc['Net Income', income_df.columns[0].strftime('%Y-%m-%d')]
+    revenue = income_df.loc['Total Revenue', income_df.columns[0].strftime('%Y-%m-%d')]
+    profit_margin = net_profit/revenue
+    return float("{:.2f}".format(profit_margin))
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python net_profit.py TICKER")
+        sys.exit(1)
+
+    ticker = sys.argv[1]
+    profit = get_profit_margin(ticker)
+    print("Quick: ", profit)
